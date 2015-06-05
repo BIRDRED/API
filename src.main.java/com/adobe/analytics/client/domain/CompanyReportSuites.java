cc76628bc957950793c1647f477755535c380e37
BIRDRED <gustt103@uol.com.br>
2015-06-05 11:24:16 -0300

    Proe

---
+++ src.main.java/com/adobe/analytics/client/domain/CompanyReportSuites.java
@@ -0,0 +1,23 @@
+package com.adobe.analytics.client.domain;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CompanyReportSuites {
+
+	private List<CompanyReportSuite> reportSuites;
+
+	public List<CompanyReportSuite> getReportSuites() {
+		return reportSuites;
+	}
+
+	public void setReportSuites(List<CompanyReportSuite> reportSuites){
+		this.reportSuites = reportSuites;
+	}
+
+	@Override
+	public String toString()  {
+		return String.format("CompanyReportSuites [reportSuites=%s]", reportSuites);
+	}
+
+}
---
+++ src.main.java/com/adobe/analytics/client/report/ReportSuiteID.java
@@ -0,0 +1,20 @@
+package com.adobe.analytics.client.report;
+
+import java.io.IOException;
+
+import com.adobe.analytics.client.AnalyticsClient;
+import com.adobe.analytics.client.domain.CompanyReportSuite;
+import com.adobe.analytics.client.domain.CompanyReportSuites;
+import com.adobe.analytics.client.method.ReportSuiteMethods;
+
+
+
+public class ReportSuiteID  
+{
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+	}
+	
+
+	
+}
